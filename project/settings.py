a47eef8f52dfea275d76b5825376bde71a2b5b38
Denzel <jdenzelmab@gmail.com>
2024-02-17 18:05:36 -0500

    Added configuration for whitenoise

--- project/settings.py
+++ project/settings.py
@@ -122,6 +122,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+WHITENOISE_USE_FINDERS = True
+
+if DEBUG:
+    WHITENOISE_AUTOREFRESH = True
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
