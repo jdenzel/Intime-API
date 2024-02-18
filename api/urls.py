590c402bbc0a3c065d7fd540a70cd528a459e0ac
Denzel <jdenzelmab@gmail.com>
2024-02-17 19:31:01 -0500

    Created urls.py and added paths to views. Included urls.py from /api/urls.py into /project/urls.py

---
+++ api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . views import *
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('checksession/', CheckSessionView.as_view(), name='checksession'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('clockin/', ClockInView.as_view(), name='clockin'),
+    path('clockout/<int:id>/', ClockOutView.as_view(), name='clockout'),
+    path('timesheet/', TimeSheetView.as_view(), name='timesheet'),
+]
