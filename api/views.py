137e8df56fbb489899fa48648dcba7f241f5cc5f
Denzel <jdenzelmab@gmail.com>
2024-02-17 19:03:00 -0500

    Added HomeView

--- api/views.py
+++ api/views.py
@@ -5,3 +5,10 @@ from rest_framework.generics import ListAPIView
 from django.contrib.auth import login, logout
 from .models import *
 from .serializers import *
+
+class HomeView(APIView):
+    def get(self, request):
+        try:
+            return Response({"message: Success!"}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_404_NOT_FOUND)
