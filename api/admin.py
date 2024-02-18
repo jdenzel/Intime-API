ff70f1746abaff11994641b3ef611de759ff980f
Denzel <jdenzelmab@gmail.com>
2024-02-17 19:32:14 -0500

    Added TimeClock model to admin for viewing database in django /admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . models import *
 
-# Register your models here.
+class TimeClockAdmin(admin.ModelAdmin):
+    list_display = ('employee', 'date', 'clock_in_time', 'clock_out_time', 'location', 'role')
+
+admin.site.register(TimeClock, TimeClockAdmin)
+
+
