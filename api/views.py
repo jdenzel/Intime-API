65e85baee76dfac0945fd48e3db24d62eb24299e
Denzel <jdenzelmab@gmail.com>
2024-02-20 16:52:06 -0500

    Added line to modify session

--- api/views.py
+++ api/views.py
@@ -18,6 +18,7 @@ from .serializers import *
 class HomeView(APIView): # Access / route | READ
     def get(self, request):
         request.session.create()
+        request.session['key'] = 'value'
         try:
             return Response({"message: Success!"}, status=status.HTTP_200_OK)
         except Exception as e:
